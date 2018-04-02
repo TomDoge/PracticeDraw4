7510f5c5aaf8ab95b036c9dc37aa92c5903c6df2
penglei <yupengniaosanshi@gmail.com>
2018-04-02 18:07:26 +0800

    实现了复杂翻页效果

--- app/src/main/java/com/hencoder/hencoderpracticedraw4/practice/Practice14FlipboardView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw4/practice/Practice14FlipboardView.java
@@ -1,7 +1,9 @@
 package com.hencoder.hencoderpracticedraw4.practice;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
-import android.animation.ValueAnimator;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -9,10 +11,9 @@ import android.graphics.Camera;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.support.annotation.Nullable;
+import android.support.v4.view.animation.FastOutSlowInInterpolator;
 import android.util.AttributeSet;
 import android.view.View;
-import android.view.animation.CycleInterpolator;
-import android.view.animation.LinearInterpolator;
 
 import com.hencoder.hencoderpracticedraw4.R;
 
@@ -20,8 +21,10 @@ public class Practice14FlipboardView extends View {
     Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
     Bitmap bitmap;
     Camera camera = new Camera();
-    int degree;
-    ObjectAnimator animator = ObjectAnimator.ofInt(this, "degree", 0, 180);
+    private int degreeZ;
+    private int degreeY;
+    private int offsetDegreeY;
+    private AnimatorSet mAnimatorSet;
 
     public Practice14FlipboardView(Context context) {
         super(context);
@@ -31,34 +34,69 @@ public class Practice14FlipboardView extends View {
         super(context, attrs);
     }
 
-    public Practice14FlipboardView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+    public Practice14FlipboardView(Context context, @Nullable AttributeSet attrs, int
+            defStyleAttr) {
         super(context, attrs, defStyleAttr);
     }
 
     {
         bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.maps);
 
-        animator.setDuration(2500);
-        animator.setInterpolator(new LinearInterpolator());
-        animator.setRepeatCount(ValueAnimator.INFINITE);
-        animator.setRepeatMode(ValueAnimator.REVERSE);
+        ObjectAnimator animator1 = ObjectAnimator.ofInt(this, "degreeY", 0, -45);
+        animator1.setDuration(1000);
+        animator1.setStartDelay(500);
+        ObjectAnimator animator2 = ObjectAnimator.ofInt(this, "degreeZ", 0, -270);
+        animator2.setDuration(1000);
+        animator2.setInterpolator(new FastOutSlowInInterpolator());
+        animator2.setStartDelay(500);
+        ObjectAnimator animator3 = ObjectAnimator.ofInt(this, "offsetDegreeY", 0, 45);
+        animator3.setDuration(1000);
+        animator3.setStartDelay(500);
+        mAnimatorSet = new AnimatorSet();
+        mAnimatorSet.playSequentially(animator1, animator2, animator3);
+        mAnimatorSet.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                reset();
+                animation.start();
+            }
+        });
     }
 
     @Override
     protected void onAttachedToWindow() {
         super.onAttachedToWindow();
-        animator.start();
+        mAnimatorSet.start();
     }
 
     @Override
     protected void onDetachedFromWindow() {
         super.onDetachedFromWindow();
-        animator.end();
+        mAnimatorSet.cancel();
+    }
+
+    public void reset() {
+        degreeZ = 0;
+        degreeY = 0;
+        offsetDegreeY = 0;
+        invalidate();
     }
 
     @SuppressWarnings("unused")
-    public void setDegree(int degree) {
-        this.degree = degree;
+    public void setDegreeY(int degreeY) {
+        this.degreeY = degreeY;
+        invalidate();
+    }
+
+    @SuppressWarnings("unused")
+    public void setOffsetDegreeY(int offsetDegreeY) {
+        this.offsetDegreeY = offsetDegreeY;
+        invalidate();
+    }
+
+    @SuppressWarnings("unused")
+    public void setDegreeZ(int degreeZ) {
+        this.degreeZ = degreeZ;
         invalidate();
     }
 
@@ -74,21 +112,28 @@ public class Practice14FlipboardView extends View {
         int y = centerY - bitmapHeight / 2;
 
         canvas.save();
-        canvas.clipRect(x, y, x + bitmapWidth, centerY);
+        canvas.translate(centerX, centerY);
+        canvas.rotate(degreeZ);
+        canvas.clipRect(0, -bitmapHeight, bitmapWidth, bitmapHeight);
+        camera.save();
+        camera.rotateY(degreeY);
+        camera.applyToCanvas(canvas);
+        camera.restore();
+        canvas.rotate(-degreeZ);
+        canvas.translate(-centerX, -centerY);
         canvas.drawBitmap(bitmap, x, y, paint);
         canvas.restore();
 
         canvas.save();
-
-        camera.save();
-        camera.rotateX(degree);
         canvas.translate(centerX, centerY);
+        canvas.rotate(degreeZ);
+        canvas.clipRect(-bitmapWidth, -bitmapHeight, 0, bitmapHeight);
+        camera.save();
+        camera.rotateY(offsetDegreeY);
         camera.applyToCanvas(canvas);
-        canvas.translate(-centerX, -centerY);
         camera.restore();
-
-        // 跟Demo不一样，先camera旋转，再切下半部分即可
-        canvas.clipRect(x, centerY, x + bitmapWidth, y + bitmapHeight);
+        canvas.rotate(-degreeZ);
+        canvas.translate(-centerX, -centerY);
         canvas.drawBitmap(bitmap, x, y, paint);
         canvas.restore();
     }
